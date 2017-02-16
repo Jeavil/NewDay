c5de9f9841044e37b9897c9649001834dbf31a7b
tangjiahui <tangjiahui@fishsaying.com>
2017-02-16 18:43:36 +0800

    Model - add data models

---
+++ NewDay/New Day/New Day/Model/MusicModel.swift
@@ -0,0 +1,18 @@
+//
+//  MusicModel.swift
+//  New Day
+//
+//  Created by Jeavil_Tang on 2017/2/16.
+//  Copyright © 2017年 Jeavil. All rights reserved.
+//
+
+import Foundation
+
+struct MusicModel {
+    
+    let name : String = ""
+    let link: String = ""
+    let duration: String = ""
+    let backgroundImgUrl : String = ""
+    
+}
---
+++ NewDay/New Day/New Day/Model/WeatherModel.swift
@@ -0,0 +1,16 @@
+//
+//  WeatherModel.swift
+//  New Day
+//
+//  Created by Jeavil_Tang on 2017/2/16.
+//  Copyright © 2017年 Jeavil. All rights reserved.
+//
+
+import UIKit
+
+struct WeatherModel {
+    let city: String = ""
+    let weather: String = ""
+    let temperature: String = ""
+    let humidity: String = ""
+}
