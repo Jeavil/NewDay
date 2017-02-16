b0cb916b53f26fe77c7a2390ecd2dd9876a56571
tangjiahui <tangjiahui@fishsaying.com>
2017-02-16 18:44:42 +0800

    UI - update UI & fixed an crash bug

--- NewDay/New Day/New Day/View/WeatherDetailView.swift
+++ NewDay/New Day/New Day/View/WeatherDetailView.swift
@@ -15,9 +15,60 @@ class WeatherDetailView: UIView {
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var weatherIcon: UIImageView!
     @IBOutlet weak var weatherLabel: UILabel!
-
     @IBOutlet weak var airQualityLabel: UILabel!
     
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
     
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    var city: NSString? {
+        get{
+        return self.cityLabel.text as NSString?
+        }
+        set {
+        self.cityLabel.text = newValue as String?;
+        }
+    }
+    
+    var temperature: NSString? {
+        get {
+            return self.temperatureLabel.text as NSString?
+        }
+        set {
+            self.temperatureLabel.text = newValue as String?
+        }
+    }
+    
+    var date:NSString? {
+        get {
+            return self.dateLabel.text as NSString?
+        }
+        set{
+            self.dateLabel.text = newValue as String?;
+        }
+    }
+    
+    var weather:NSString? {
+        get {
+            return self.weatherLabel.text as NSString?
+        }
+        set {
+            self.weatherLabel.text = newValue as String?
+        }
+    }
+    
+    var airQuality:NSString? {
+        get {
+            return self.airQualityLabel.text as NSString?
+        }
+        set {
+            self.airQualityLabel.text = newValue as String?
+        }
+    }
 
+    
 }
